2cfdaa81b0334be8b56b1d393948389e32808a32
jrodrigomg <jrodrigomg@hotmail.com>
2018-05-26 22:40:09 -0600

    Send telegram message.. (heroku app)

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var Alexa =  require('alexa-sdk');
+var https = require('https');
+
+//Configuration managment
+const config = require('./config.json');
 
 var SKILL_NAME = "Super Skill";
 
@@ -11,12 +15,29 @@ var MY_SUPER_POWER_LIST = [
 
 
 
+
 var handlers = {
     "MySuperSkillIntent" : function(){
         var str_index = this.event.request.intent.slots.order.resolutions.resolutionsPerAuthority[0].values[0].value.id;
         var index = Number(str_index);
         var power = MY_SUPER_POWER_LIST[index];
         this.emit(":tell","Hi Rodrigo, my power is " + power);
+    },
+    "TelegramMessageIntent":async function(){
+
+        await new Promise ((resolve, reject) => {
+            let req = https.get(config.telegram_url + 'newhi/');
+        
+            req.on('response', res => {
+                resolve(res);
+            });
+        
+            req.on('error', err => {
+                reject(err);
+            });
+        }); 
+
+        this.emit(":tell","Sent!");
     }
 }
 
